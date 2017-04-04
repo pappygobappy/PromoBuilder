e39cb72bc04a42ec4b9bfe244a72bbc1ad918938
pappygobappy <pappygobappy@gmail.com>
2017-04-04 10:03:45 -0700

    Fixed loading bug

--- src/app/containers/loading/loading.component.ts
+++ src/app/containers/loading/loading.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loading',
@@ -8,11 +8,11 @@ import { Location } from '@angular/common';
   styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent implements OnInit {
-  constructor(public af: AngularFire, private location: Location) { 
+  constructor(public af: AngularFire, private router: Router) { 
     this.af.auth.subscribe(user => {
   		console.log(user);
   		if(user){
-  			this.location.back();
+  			this.router.navigate(['']);
   		}
   	});
   }
